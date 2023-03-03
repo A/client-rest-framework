065f09806965a54929b47d1b0feb5d5a52e4b89e
Shuvalov Anton <anton@shuvalov.info>
2023-03-03 12:42:50 +0700

    Fix append slash list url bug (#5)

--- src/api/BaseAPI.spec.ts
+++ src/api/BaseAPI.spec.ts
@@ -9,7 +9,7 @@ describe('BaseAPI', () => {
     }
 
     // @ts-ignore
-    expect(new API().buildListURL({})).toEqual("/users")
+    expect(new API().buildListURL({})).toEqual("/users/")
   })
 
   it('should build correct detail url', () => {
@@ -29,5 +29,15 @@ describe('BaseAPI', () => {
     // @ts-ignore
     expect(new API({ appendSlash: false }).buildDetailURL({ urlParams: { pk: 1 }}))
       .toEqual("/users/1")
+  })
+
+  it('shouldn\'t add tailing slash to list URL when `appendSlash` is disabled', () => {
+    class API extends BaseAPI {
+      url = "/users"
+    }
+
+    // @ts-ignore
+    expect(new API({ appendSlash: false }).buildListURL({ urlParams: { pk: 1 }}))
+      .toEqual("/users")
   })
 });
